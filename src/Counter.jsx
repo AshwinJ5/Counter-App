539da842d1ab05c6da9e7c6aa2ffc630fc5d43b0
AshwinJ5 <ashwinjoseph03@gmail.com>
2024-04-05 07:07:14 +0530

    Counter App

---
+++ src/Counter.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Counter() {
+
+    const[counter,setCounter]=useState(0)
+
+function increment(){
+    setCounter(counter+1)
+}
+function decrement(){
+    if (counter>0){
+    setCounter(counter-1)}
+    
+}
+function reset(){
+    setCounter(0)
+}
+
+
+
+  return (
+    <div className='text-center mt-5'>
+            <h1 className='text-primary'>Counter-App</h1>
+            <h1>{counter}</h1>
+            <div className='align-items-center'>
+                <button onClick={increment}  className='btn btn-primary m-2'>Increment</button>
+                <button onClick={reset}  className='btn btn-warning m-2'>Reset</button>
+                <button onClick={decrement} className='btn btn-danger m-2'>Decrement</button>
+            </div>
+    </div>
+  )
+}
+
+export default Counter
